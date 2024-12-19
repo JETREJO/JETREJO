7351a2a1f9660f9501d1df8a62481c2df0b09672
Javier Trejo <javiertrejorod@gmail.com>
2024-12-19 00:46:12 -0600

    feat: add responsiveness on experience section

--- src/components/experience/cardLink/cardLink.tsx
+++ src/components/experience/cardLink/cardLink.tsx
@@ -11,7 +11,7 @@ const ExperienceCardLink: React.FC<CardLinkProps> = ( {url, client} ) => {
     if (url) {
       return (
         <a href={url} target="_blank" rel="noopener noreferrer" className={styles.cardLink}>
-          <span>Go to page </span>
+          <span style={{whiteSpace: "nowrap"}}>Go to page </span>
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>
         </a>
       );
